c6f341afca847220536e6ed94512dcb6d5ae7c9f
kernc <github-actions@github.com>
2020-11-04 00:31:37 +0000

    CI: Update 4372ca2e4c66cc9e3c3f95c35e453de83a87e02b

--- static/script.js
+++ static/script.js
@@ -1,1 +1,1 @@
-"use strict";if(location.hash=='#menu');location.hash='';function toggleMenu(visible,ev){ev.preventDefault();if(visible);location.hash='menu';else;history.back();}
+"use strict";(()=>{const toggle=document.getElementById('menu-toggle');toggle.checked=false;toggle.addEventListener('keyup',ev=>{if(ev.code==='Enter');toggle.checked=!toggle.checked;if(ev.code==='Escape');toggle.checked=false;});document.getElementById('menu').addEventListener('keyup',ev=>{if(ev.code==='Escape');toggle.checked=false;});})();
